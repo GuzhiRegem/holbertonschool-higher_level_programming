58391d97da4472130120ce241682eefeb56ebba2
Seba <3292@holbertonschool.com>
2021-09-07 17:17:53 +0000

    commit

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+    for num in range(100):
+        num += 1
+        out = ""
+        if (num % 3) == 0:
+            out += "Fizz"
+        if (num % 5) == 0:
+            out += "Buzz"
+        if out == "":
+            out = str(num)
+        print("{} ".format(out), end="")
+    print()
+fizzbuzz()
