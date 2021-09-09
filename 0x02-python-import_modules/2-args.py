2d9ce8af6c440574bf928bdc6af1700fffeba7b6
Seba <3292@holbertonschool.com>
2021-09-09 16:47:16 +0000

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__" and sys.argv:
-    args = sys.argv[1:]
-    arguments = len(args)
+    args = sys.argv
+    arguments = len(args) - 1
     word = "argument"
     if arguments != 1:
         word += "s"
     print("{} {}{}".format(arguments, word, ":" if arguments else "."))
-    for indx in range(len(args)):
-        print("{}: {}".format(indx + 1, args[indx]))
+    indx = 1
+    while indx <= arguments:
+        print("{}: {}".format(indx, args[indx]))
+        indx += 1
