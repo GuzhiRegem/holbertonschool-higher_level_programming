203d744ee923b56eeb93583c5f89b2514e21f0e7
Seba <3292@holbertonschool.com>
2021-09-09 15:27:35 +0000

    commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
-!/usr/bin/python3
-import add_0 as mo
-a = 1
-b = 2
+#!usr/bin/python3
 if __name__ == "__main__":
+    import add_0 as mo
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, mo.add(a, b)))
