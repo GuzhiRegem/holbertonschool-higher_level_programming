c975dd883bb6acdd71488cbdd022d341240f83a6
Seba <3292@holbertonschool.com>
2021-09-13 16:14:18 +0000

    commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,8 +3,10 @@
 
 def print_reversed_list_integer(my_list=[]):
     lar = len(my_list)
+    if lar == 0:
+        print()
     for idx in range(lar):
         print("{:d}".format(my_list[-(idx + 1)]))
 
-my_list = [1,2]
+my_list = [1,2,3]
 print_reversed_list_integer(my_list)
