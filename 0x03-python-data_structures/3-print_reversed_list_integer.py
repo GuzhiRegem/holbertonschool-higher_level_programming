43d53bf28d18ac766fa6702d35bf38a0f6f78500
Seba <3292@holbertonschool.com>
2021-09-13 17:38:57 +0000

    commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for idx in my_list:
-        print("{:d}".format(idx))
+    if my_list != None:
+        my_list.reverse()
+        for idx in my_list:
+            print("{:d}".format(idx))
