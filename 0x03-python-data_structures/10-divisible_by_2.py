4f46f0f6a3087c5e951a697c1ea41f700140c322
Seba <3292@holbertonschool.com>
2021-09-13 16:12:04 +0000

    commit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,5 +4,5 @@
 def divisible_by_2(my_list=[]):
     out = []
     for ele in my_list:
-        out.append(ele % 2 == 1)
+        out.append(bool(ele % 2 == 1))
     return out
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,3 +5,6 @@ def print_reversed_list_integer(my_list=[]):
     lar = len(my_list)
     for idx in range(lar):
         print("{:d}".format(my_list[-(idx + 1)]))
+
+my_list = [1,2]
+print_reversed_list_integer(my_list)
