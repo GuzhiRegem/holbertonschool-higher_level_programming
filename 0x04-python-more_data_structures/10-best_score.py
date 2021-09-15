1be5823301c132cd19c578cbe79a7047df9c7f22
Sebastian <3292@holbertonschool.com>
2021-09-15 10:38:55 -0700

    commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,9 @@
 
 def best_score(a_dictionary):
         out = list(a_dictionary.items())[0][1]
+        key = ""
         for i in a_dictionary.items():
                 if i[1] > out:
                         out = i[1]
-        return out
+                        key = i[0]
+        return key
