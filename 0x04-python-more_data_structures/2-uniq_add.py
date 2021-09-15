3646c36f011289dbf417e1af1bb066aadefe0e30
Sebastian <3292@holbertonschool.com>
2021-09-15 09:45:36 -0700

    commit

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+        uniqs = []
+        out = 0
+        for i in my_list:
+                if i not in uniqs:
+                        uniqs.append(i)
+                        out += i
+        return out
