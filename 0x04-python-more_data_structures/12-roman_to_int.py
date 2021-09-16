0cf96bef8e00a32c56d4b21e4c942145314eed55
Sebastian <3292@holbertonschool.com>
2021-09-15 18:29:36 -0700

    commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,9 +7,12 @@ def roman_to_int(roman_string):
         val = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         for i in roman_string:
                 lis.append(val[i])
+        rm = []
         for i in range(len(lis)):
+                print (lis[i])
                 if lis[i] > lis[i - 1 if i > 0 else 0]:
                         lis[i] = lis[i] - lis[i-1]
-                        del lis[i-1]
-                        i -= 1
+                        rm.append(i - 1)
+        for i in range(len(rm)):
+                del lis[rm[i] - i]
         return sum(lis)
