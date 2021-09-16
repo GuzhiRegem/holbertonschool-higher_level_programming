ac14eca62b212d453cc9525086a951fb080fbf0c
Sebastian <3292@holbertonschool.com>
2021-09-15 17:37:04 -0700

    commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,6 @@
 
 def best_score(a_dictionary):
         out = None
-        if len(a_dictionary) > 0:
+        if a_dictionary and a_dictionary != {}:
                 out = max(a_dictionary, key=a_dictionary.get)
         return out
-
