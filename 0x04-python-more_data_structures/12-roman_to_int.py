9b92e0a3ae1f5807b9ea12b7bb11f6f962f6e200
Sebastian <3292@holbertonschool.com>
2021-09-15 18:38:26 -0700

    commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-        if isinstance(roman_string, str):
+        if not isinstance(roman_string, str):
                 return None
         out = 0
         lis = []
