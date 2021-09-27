d2de8fa3cc5e3f5c2ae2d76fd17b1b01cdb7bd1f
Guzhi <3292@holbertonschool.com>
2021-09-27 09:09:21 -0700

    commit

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-    while result < 10:
-        result += 1
-        print(result)
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += (a ** b) / i
+        except:
+            result = b + a
+            break
     return result
