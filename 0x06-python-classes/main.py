0616a003b2ff3118d7ed7290cc9621696226b10b
Guzhi <3292@holbertonschool.com>
2021-09-29 14:57:45 -0700

    commit

--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 import dis
-magic = __import__('103-magic_class').MagicClass
-dis.dis(magic)
+MagicClass = __import__('103-magic_class').MagicClass
+mc = MagicClass(10) 
+print("{:.2f}".format(mc.circumference()))
+print("{:.2f}".format(mc.area()))
