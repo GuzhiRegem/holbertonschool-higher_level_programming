d1cf226a810d028b678ff018b4485d8dcb107b99
Guzhi <3292@holbertonschool.com>
2021-09-29 12:16:49 -0700

    commit

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,4 +1,38 @@
 #!/usr/bin/python3
+"""
+    4-square
+    que alguien me mate
+    return {}
+"""
 
 
+@total_ordering
+class Square:
+    """
+        square
+    """
+    def __init__(self, size=0):
+        """ init """
+        self.__size = size
+
+    def area(self):
+        """ area """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ size """
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def __eq__(self, other):
 
