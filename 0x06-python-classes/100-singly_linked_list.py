02dc5b969458c1b04998859bd21029ec7f6bda0b
Guzhi <3292@holbertonschool.com>
2021-09-29 10:07:58 -0700

    commit

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,27 +1,95 @@
 #!/usr/bin/python3
+"""
+    100-singly_linked_list
+    damn
+    return {}
+"""
 
 
 class Node:
+    """
+        Node
+    """
     def __init__(self, data, next_node=None):
+        """ arro """
         self.__data = data
         self.__next_node = next_node
     
     @property
     def data(self):
+        """ data """
         return self.__data
+
     @data.setter
     def data(self, value):
+        """ data """
         if isinstance(value, int):
             self.__data = value
         else:
             raise TypeError("data must be an integer")
+
     @property
     def next_node(self):
+        """ next_node """
         return self.__next_node
+
     @next_node.setter
     def next_node(self, value):
+        """ next_node """
         if isinstance(value, Node) or value is None:
             self.__next_node = value
         else:
             raise TypeError("next_node must be a Node object")
 
+class SinglyLinkedList:
+    """
+        SinglyLinkedList
+    """
+    def __init__(self):
+        """ init """
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """ sorted_insert """
+        try:
+            node = Node(value, None)
+        except TypeError:
+            return
+        if self.__head == None:
+            self.__head = node
+        else:
+            ptr = self.__head
+            if ptr.data > value:
+                node.next_node = self.__head
+                self.__head = node
+                return
+            while ptr.next_node != None:
+                if ptr.next_node.data >= value:
+                    break
+                ptr = ptr.next_node
+            node.next_node = ptr.next_node
+            ptr.next_node = node
+
+    def __str__(self):
+        """ str """
+        he = self.__head
+        if he == None:
+            return ""
+        while he.next_node is not None:
+            print("{}".format(he.data))
+            he = he.next_node
+        return str(he.data)
+
+sll = SinglyLinkedList()
+sll.sorted_insert(2)
+sll.sorted_insert(5)
+sll.sorted_insert(3)
+sll.sorted_insert(10)
+sll.sorted_insert(1)
+sll.sorted_insert(-4)
+sll.sorted_insert(-3)
+sll.sorted_insert(4)
+sll.sorted_insert(5)
+sll.sorted_insert(12)
+sll.sorted_insert(3)
+print(sll)
