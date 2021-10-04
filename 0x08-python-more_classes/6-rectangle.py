b8e6b493a32db1458d048a0fc163ed15d5b260fe
Guzhi <3292@holbertonschool.com>
2021-10-04 14:37:22 -0700

    commit

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = height
-        self.number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -87,4 +87,4 @@ class Rectangle:
     def __del__(self):
         """ del """
         print("Bye rectangle...")
-        self.number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
