22093681821a5e7753fa74717d840ede9bcd405f
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-10-06 07:48:29 -0700

    commit

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,30 +5,30 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-	def test1(self):
-		""" muajajaja """
-		self.assertEqual(max_integer([1, 2, 3]), 3)
-	
-	def test2(self):
-		""" muajajaja """
-		self.assertEqual(max_integer([3, 4, 3]), 4)
-	
-	def test3(self):
-		""" muajajaja """
-		self.assertEqual(max_integer([1, 0, 0]), 1)
-	
-	def test4(self):
-		""" muajajaja """
-		self.assertEqual(max_integer([]), None)
-	
-	def test6(self):
-		""" muajajaja """
-		self.assertEqual(max_integer([1, 2, -3]), 2)
+    def test1(self):
+        """ muajajaja """
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+
+    def test2(self):
+        """ muajajaja """
+        self.assertEqual(max_integer([3, 4, 3]), 4)
+
+    def test3(self):
+        """ muajajaja """
+        self.assertEqual(max_integer([1, 0, 0]), 1)
+
+    def test4(self):
+        """ muajajaja """
+        self.assertEqual(max_integer([]), None)
 
     def test6(self):
-		""" muajajaja """
-		self.assertEqual(max_integer([-1, -2, -3]), -1)
+        """ muajajaja """
+        self.assertEqual(max_integer([1, 2, -3]), 2)
 
-    def test6(self):
-		""" muajajaja """
-		self.assertEqual(max_integer([1]), 1)
+    def test7(self):
+        """ muajajaja """
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+
+    def test8(self):
+        """ muajajaja """
+        self.assertEqual(max_integer([1]), 1)
