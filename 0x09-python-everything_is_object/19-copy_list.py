ff1e8fdfc49e07937f67f3f6a0fa2c9cbfbd9130
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-10-06 06:15:26 -0700

    commit

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-"""
-    esto es una documentacion seria sobre un modulo
-    bueno javi me lo tomo enserio
-    return a new list
-"""
-
-
 def copy_list(l):
-    """ copies a list """
     return l[:]
