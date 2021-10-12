c1ada1f67aef8a5b8fb70133b6ed5c522b33d34b
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-10-12 10:06:03 -0700

    commit

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+    7-base_geometry.py
+    module
+    return: nothing
+"""
+
+
+class BaseGeometry:
+    """ BaseGeometry """
+    def area(self):
+        """ area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ integer validator """
+        if type(value) != int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """ Rectangle """
+    def __init__(self, width, height):
+        """ init """
+        try:
+            super().integer_validator("width", width)
+            self.__width = width
+        except Exception as e:
+            raise e
+        try:
+            super().integer_validator("height", height)
+            self.__height = height
+        except Exception as e:
+            raise e
+
+    def area(self):
+        """ area """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ str """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+class Square(Rectangle):
+    """ Square """
+    def __init__(self, size):
+        """ init """
+        try:
+            super().integer_validator("size", size)
+            self.__height = size
+            self.__width = size
+        except Exception as e:
+            raise e
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@
 
 def inherits_from(obj, a_class):
     """ True if the object is an instance of a class that inherited """
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
