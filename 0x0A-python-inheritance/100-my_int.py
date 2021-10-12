836b040836aafae4ceed0ffa85b0d40588f865ba
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-10-12 10:48:24 -0700

    commit

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -9,7 +9,9 @@
 class MyInt(int):
     """ MyInt """
     def __eq__(self, other):
+        """ eq """
         return self.real != other.real
 
     def __ne__(self, other):
+        """ ne """
         return self.real == other.real
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+	101-add_attribute.py
+	module
+	return: nothing
+"""
+
+
+def add_attribute()
