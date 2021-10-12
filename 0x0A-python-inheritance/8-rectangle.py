85df0826042ff580e652b1254c2e24c961280655
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-10-12 09:49:28 -0700

    commit

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    7-base_geometry.py
+    module
+    return: nothing
+"""
+
+
+class BaseGeometry:
+    """ BaseGeometry """
+    def area(self):
+        """ area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ integer validator """
+        if type(value) != int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """ Rectangle """
+    def __init__(self, width, height):
+        """ init """
+        try:
+            super().integer_validator("width", width)
+            self.__width = width
+        except Exception as e:
+            raise e
+        try:
+            super().integer_validator("height", height)
+            self.__height = height
+        except Exception as e:
+            raise e
