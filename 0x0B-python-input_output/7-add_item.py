a9f8b99fd0e5b014f20591cd8b2fe111c4ee5b63
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-10-13 16:31:59 -0700

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,10 +13,10 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 lis = list(sys.argv)[1:]
+out = []
 try:
-    lis1 = load_from_json_file("add_item.json")
-    for a in lis1:
-        lis.insert(0, a)
+    out = list(load_from_json_file("add_item.json"))
 except Exception as e:
     pass
-save_to_json_file(lis, "add_item.json")
+out = out + lis
+save_to_json_file(out, "add_item.json")
