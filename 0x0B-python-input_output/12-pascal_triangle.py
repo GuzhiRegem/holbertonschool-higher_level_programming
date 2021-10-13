bb4854c7ac872a8d526a7d2a2df9566f9db77176
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-10-13 10:33:35 -0700

    commit

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    12-pascal_triangle.py
+    module
+    return: nothing
+"""
+
+
+def pascal_triangle(n):
+    """ pascal """
+    lis = [[1]]
+    if n <= 0:
+        return lis
+    for i in range(n - 1):
+        tmp = [0] * (len(lis[-1]) + 1)
+        for num in range(len(lis[-1])):
+            tmp[num] += lis[-1][num]
+            tmp[num + 1] += lis[-1][num]
+        lis.append(tmp)
+    return lis
