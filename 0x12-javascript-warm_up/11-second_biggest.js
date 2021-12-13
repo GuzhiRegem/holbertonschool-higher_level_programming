240b93a3ecf1fc325f9d22d9596939f2def954e0
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-12-13 13:48:44 -0300

    commit

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
 
+const { argv } = require('process');
+let i;
+let max = parseInt(argv[2]);
+let second = 0;
+if (!isNaN(parseInt(argv[3]))) {
+  second = parseInt(argv[3]);
+}
+for (i = 3; argv[i]; i++) {
+  const num = parseInt(argv[i]);
+  if (num > max) {
+    second = max;
+    max = num;
+  }
+}
+console.log(second);
