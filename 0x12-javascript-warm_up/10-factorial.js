2b9381848395d2d745a69b8d2c06bb086dde4f1c
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-12-13 13:32:54 -0300

    commit

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
 
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n < 3) {
+    return n;
+  }
+  return factorial(n - 1) * n;
+}
+const { argv } = require('process');
+const num1 = parseInt(argv[2]);
+console.log(factorial(num1));
