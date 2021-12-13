6d9120c7b481f29a3fc460182fc647ef0c19f740
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-12-13 13:04:52 -0300

    commit

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 
+const { argv } = require('process');
+const amnt = parseInt(argv[2]);
+let i;
+if (isNaN(amnt)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (i = 0; i < amnt; i++) {
+    console.log('C is fun');
+  }
+}
