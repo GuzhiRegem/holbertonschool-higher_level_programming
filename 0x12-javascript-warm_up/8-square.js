dbe84fd19956cbc6d01b1eb630557894dca04152
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-12-13 13:07:44 -0300

    commit

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
 
+const { argv } = require('process');
+const amnt = parseInt(argv[2]);
+let x, y;
+if (isNaN(amnt)) {
+  console.log('Missing size');
+} else {
+  for (y = 0; y < amnt; y++) {
+    let out = '';
+    for (x = 0; x < amnt; x++) {
+      out += 'X';
+    }
+    console.log(out);
+  }
+}
