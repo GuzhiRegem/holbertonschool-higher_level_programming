325522f0566b1af30b2ac2b2aa23b443c8ab2396
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-12-13 12:57:17 -0300

    commit

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
 
+const { argv } = require('process');
+const out = parseInt(argv[2]);
+if (isNaN(out)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + out);
+}
