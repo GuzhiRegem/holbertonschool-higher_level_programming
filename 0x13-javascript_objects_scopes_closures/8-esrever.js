50ebbc8a95990e7cfdc65367f281ff06cff0e420
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-12-14 16:22:40 -0300

    8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,3 +1,11 @@
 #!/usr/bin/node
 
-exports.esrever = function (list)
+exports.esrever = function (list) {
+  const out = [];
+  let i;
+  const len = list.length;
+  for (i = len - 1; i >= 0; i--) {
+    out.push(list[i]);
+  }
+  return out;
+};
