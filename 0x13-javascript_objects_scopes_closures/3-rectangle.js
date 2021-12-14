4b663f2c4977be44bf8a18323a2c23f275f5a229
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-12-14 15:54:35 -0300

    3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (!(Number.isInteger(w)) || !(Number.isInteger(h))) {
+      return;
+    }
+    if ((w <= 0) || (h <= 0)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let xP;
+    let yP;
+    for (yP = 0; yP < this.height; yP++) {
+      let rowPrint = '';
+      for (xP = 0; xP < this.width; xP++) {
+        rowPrint += 'X';
+      }
+      console.log(rowPrint);
+    }
+  }
+};
