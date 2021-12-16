f7409eb09fbe66257256b80ae5c35f9df317cc67
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-12-16 11:40:22 -0300

    commit

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,16 +4,17 @@
     module
     return: nothing
 """
-import MySQLdb
-from sys import argv
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
 
-if len(argv) < 4:
-    raise ValueError("must have 4 args")
-conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    if len(argv) < 4:
+        raise ValueError("must have 4 args")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,9 +12,11 @@ if __name__ == '__main__':
         raise ValueError("must have 5 args")
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
+    cur.execute("SELECT cities.name, FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = {} ORDER BY cities.id ASC".format(argv[4]))
     query_rows = cur.fetchall()
+    names = []
     for row in query_rows:
-        print(row)
+        names.append(row[0])
+    print(*names, sep=", ")
     cur.close()
     conn.close()
