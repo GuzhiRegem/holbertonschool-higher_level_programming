bc7dc9754ae119cdd5d7ab3d41b24336e076187e
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-12-16 06:25:48 -0800

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    0-select_states.py
+    module
+    return: nothing
+"""
+import MySQLdb
+from sys import argv
+
+if len(argv) < 5:
+    raise ValueError("must have 5 args")
+conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT name FROM states")
+query_rows = cur.fetchall()
+names = []
+for row in query_rows:
+    names.append(row[0])
+if argv[4] in names:
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+cur.close()
+conn.close()
