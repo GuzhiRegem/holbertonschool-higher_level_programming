f41ef356d0399db5555a869a3f1d5bda64a64749
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-12-16 06:03:54 -0800

    commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    0-select_states.py
+    module
+    return: nothing
+"""
+import MySQLdb
+from sys import argv
+
+if len(argv) < 4:
+    raise ValueError("must have 4 args")
+conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
