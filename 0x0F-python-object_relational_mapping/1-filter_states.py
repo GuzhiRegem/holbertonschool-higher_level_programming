c8dce25ae543b792d25ea0427ee6e8b60ac18e42
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-12-18 17:11:39 -0800

    commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,10 @@ if __name__ == '__main__':
 
     if len(argv) < 4:
         raise ValueError("must have 4 args")
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+    conn = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=argv[1], passwd=argv[2],
+        db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,9 +10,18 @@ if __name__ == '__main__':
 
     if len(argv) < 5:
         raise ValueError("must have 5 args")
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+    conn = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=argv[1], passwd=argv[2],
+        db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
+    cur.execute(
+        """
+        SELECT *
+        FROM states
+        WHERE name = '{}'
+        ORDER BY id ASC
+        """.format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
