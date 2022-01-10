0e2abca2f193ec708bd1427e892cdf8adb52d345
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-10 17:02:42 -0300

    commit

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ import sys
 
 
 if __name__ == "__main__":
-    data = {"q":""}
+    data = {"q": ""}
     if len(sys.argv) > 1:
         data["q"] = sys.argv[1]
     r = requests.post("http://0.0.0.0:5000/search_user", data=data)
