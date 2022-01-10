0afed6d4667ad1a1b2409de2c61d529b23b93d3f
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-10 15:47:40 -0300

    commit

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,4 +9,8 @@ import sys
 
 
 if __name__ == "__main__":
-
+    try:
+        r = requests.get(sys.argv[1])
+        print(r.text)
+    except requests.HTTPError as e:
+        print("Error code: {}".format(e.status_code))
