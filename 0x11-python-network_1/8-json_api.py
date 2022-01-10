d3debf27d10ef13433c7166b1dd2eaf5299b737b
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-10 16:15:00 -0300

    commit

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,10 +10,10 @@ import sys
 
 if __name__ == "__main__":
     q = "" if len(sys.argv) <= 1 else sys.argv[1]
-    r = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+    r = requests.post("http://0.0.0.0:5000/search_user", {"q": q})
     try:
         d = r.dict()
-        if len(r) > 0:
+        if r:
             print("[{}] {}".format(r.get("id"), r.get("name")))
         else:
             print("No result")
