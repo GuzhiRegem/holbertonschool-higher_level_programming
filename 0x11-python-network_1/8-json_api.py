5a58cb1c94357f7a3aaca347845059cb0e7cf838
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-10 16:01:13 -0300

    commit

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,4 +9,13 @@ import sys
 
 
 if __name__ == "__main__":
-
+    q = "" if len(sys.argv) <= 1 else sys.argv[1]
+    r = requests.post(sys.argv[1], data={"q":q})
+    try:
+        d = r.dict()
+        if len(r) > 0:
+            print("[{}] {}".format(r.get("id"), r.get("name")))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
