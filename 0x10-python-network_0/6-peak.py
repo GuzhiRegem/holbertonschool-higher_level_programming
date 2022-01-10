81f94084d2bbd1332d0f51328e06bf42ebecfa9c
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-10 13:02:53 -0300

    commit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,15 +5,16 @@
     return: nothing
 """
 
+
 def find_peak(list_of_integers):
     """ find peak """
     li = list_of_integers
-    l = len(li)
-    if l == 0:
+    li1 = len(li)
+    if li1 == 0:
         return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
+    m = li1 // 2
+    if (m == li1 - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
         return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
+    if m != li1 - 1 and li[m + 1] > li[m]:
         return find_peak(li[m + 1:])
     return find_peak(li[:m])
