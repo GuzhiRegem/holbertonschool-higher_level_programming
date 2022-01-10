98af720112c86c6f301abb343ce201255e60a29b
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-10 13:58:16 -0300

    commit

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,4 +9,8 @@ import sys
 
 
 if __name__ == "__main__":
-
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
