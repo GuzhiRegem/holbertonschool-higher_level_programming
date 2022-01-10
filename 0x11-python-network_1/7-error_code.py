bafbd2ab62275a2fef3f6ef39b04cbf8e13dde07
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-10 15:52:36 -0300

    commit

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,8 +9,6 @@ import sys
 
 
 if __name__ == "__main__":
-    try:
-        r = requests.get(sys.argv[1])
-        print(r.text)
-    except requests.HTTPError as e:
-        print("Error code: {}".format(e.status_code))
+    r = requests.get(sys.argv[1])
+    cod = r.status_code
+    print(r.text if cod < 400 else "Error code: {}".format(cod))
