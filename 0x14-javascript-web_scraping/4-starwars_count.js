5dbfd9bb590d3e45c9edcad3508e34563aa65d9e
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-02-01 13:13:20 -0300

    commit

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const search = 'https://swapi-api.hbtn.io/api/people/18/';
+request(url, function (error, response, body) {
+  if (!error) {
+    let count = 0;
+    const results = JSON.parse(body).results;
+    for (let i = 0; results[i]; i++) {
+      if (results[i].characters.includes(search)) {
+        count += 1;
+      }
+    }
+    console.log(count);
+  }
+});
