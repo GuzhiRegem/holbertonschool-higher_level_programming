423118d4c6e09ef602afecb963bd54c962fc2a4c
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-02-01 12:47:38 -0300

    commit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    console.log('statusCode:', response && response.statusCode);
+  }
+});
