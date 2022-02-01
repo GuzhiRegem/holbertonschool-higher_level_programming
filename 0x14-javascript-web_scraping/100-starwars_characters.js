adac79ca2d853033b77454c184fdbd5f2aa6a8b8
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-02-01 15:24:54 -0300

    commit

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+const request = require('request');
+const num = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films';
+request(url, function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    for (let i = 0; results[i]; i++) {
+      if (i + 1 === parseInt(num)) {
+        const chars = results[i].characters;
+        for (let e = 0; chars[e]; e++) {
+          request(chars[e], function (error, response, body) {
+            if (!error) {
+              console.log(JSON.parse(body).name);
+            }
+          });
+        }
+      }
+    }
+  }
+});
