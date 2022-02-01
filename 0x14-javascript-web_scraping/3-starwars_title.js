8211904b1a7fb04620d94851800f33a076bd8280
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-02-01 13:02:01 -0300

    commit

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+const num = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films';
+request(url, function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    for (let i = 0; results[i]; i++) {
+      if (results[i].episode_id === parseInt(num)) {
+        console.log(results[i].title);
+      }
+    }
+  }
+});
