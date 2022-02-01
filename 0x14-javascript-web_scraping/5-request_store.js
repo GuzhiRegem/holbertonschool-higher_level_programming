fd9170b55b69eaaae6821484d83a6d1f070a0db0
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-02-01 13:17:27 -0300

    commit

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filename = process.argv[3];
+request(url, function (error, response, body) {
+  if (!error) {
+    fs.writeFile(filename, body, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
