2d63788e497c32a740f9c0ca97cc24c578c0c20c
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-02-01 14:49:10 -0300

    commit

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,13 +3,15 @@ const request = require('request');
 const url = process.argv[2];
 request(url, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const out = {
-      1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0
-    };
+    const out = {};
     const list = JSON.parse(body);
     for (let i = 0; list[i]; i++) {
       if (list[i].completed) {
-        out[list[i].userId]++;
+        if (out[list[i].userId] === undefined) {
+          out[list[i].userId] = 1;
+        } else {
+          out[list[i].userId]++;
+        }
       }
     }
     console.log(out);
